49ff53f9273933e610b56319869dd9f86926fba9
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-16 15:31:56 +0530

    Register Page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./register.scss";
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+        <div className="left">
+          <h1>Social App</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sit
+            enim, nihil laboriosam ducimus eius magni saepe tenetur ratione cum
+            rem deserunt provident reprehenderit illo eos ad neque sapiente et?
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
